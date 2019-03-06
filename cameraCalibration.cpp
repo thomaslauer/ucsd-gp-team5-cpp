ffafb66674754b934a52a8301294f35fd587dfa8
thomaslauer <thomaslauer01@gmail.com>
2019-03-06 00:25:11 -0800

    added chessboard detection

--- cameraCalibration.cpp
+++ cameraCalibration.cpp
@@ -21,10 +21,34 @@ int main(int argc, char** argv) {
     string path = argv[1];
     string outputFile = argv[2];
     
+    Mat coverage(FRAME_HEIGHT, FRAME_WIDTH, CV_8UC3, Scalar(0,0,0));
     for(auto& entry : boost::make_iterator_range(directory_iterator(path), {})) {
         cout << "loading file " << entry << endl;
-        Mat image = imread(entry.path().string());
-        imshow("img", image);
-        waitKey(0);
+        Mat img = imread(entry.path().string());
+        Size imageSize(FRAME_WIDTH, FRAME_HEIGHT);
+        resize(img, img, imageSize);
+        Mat gray;
+        cvtColor(img, gray, CV_BGR2GRAY);
+        
+        Size patternSize(CHESSBOARD_WIDTH, CHESSBOARD_HEIGHT);
+        vector<Point2f> corners;
+
+        bool patternFound = findChessboardCorners(gray, patternSize, corners, 
+                CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
+                + CALIB_CB_FAST_CHECK);
+
+        if(patternFound) {
+            cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1),
+                    TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+
+            drawChessboardCorners(img, patternSize, Mat(corners), patternFound);
+            drawChessboardCorners(coverage, patternSize, Mat(corners), patternFound);
+        }
+
+        if(USE_GUI) {
+            imshow("img", img);
+            if(waitKey(1000) == KEY_ESC) break;
+        }
     }
+    imwrite("coverage.png", coverage);
 }
--- include/constants.hpp
+++ include/constants.hpp
@@ -7,4 +7,11 @@
 #define FRAME_RATE 40
 #define BUFFER_SIZE 3
 
+#define CHESSBOARD_WIDTH 7
+#define CHESSBOARD_HEIGHT 10
+
+#define KEY_ESC 27
+
+#define USE_GUI false
+
 #endif
