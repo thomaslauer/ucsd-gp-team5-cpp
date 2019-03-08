7883f3a03201ee8498c8d226fd17456578211446
Thomas Lauer <tlauer@ucsd.edu>
2019-03-07 18:30:40 -0800

    added fps monitor

--- preview.cpp
+++ preview.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "opencv2/opencv.hpp"
+#include <time.h>
 
 #include "constants.hpp"
 #include "calibration.hpp"
@@ -19,14 +20,20 @@ int main(int argc, char** argv) {
     Calibration cb;
     cb.load(argv[1]);
 
+    clock_t t;
     Mat cameraFrame;
     while(true) {
+        t = clock();
         stream.read(cameraFrame);
-        cvtColor(cameraFrame, cameraFrame, CV_BGR2GRAY);
-        //cb.undistort(cameraFrame);
+        cb.undistort(cameraFrame);
         cb.hTransform(cameraFrame);
         imshow("frame", cameraFrame);
         if(waitKey(1) == KEY_ESC) break;
+
+        t = clock() - t;
+        double time_taken = ((double)t) / CLOCKS_PER_SEC;
+        cout << 1/time_taken << " fps" << endl;
+
     }
 
     return 0;
